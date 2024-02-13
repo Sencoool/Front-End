857d196f358368dae9577b9ea82d9687e645e618
Sencoool <s6506021610037@email.kmutnb.ac.th>
2024-02-13 08:54:18 +0700

    Updated

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -8,7 +8,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-const base_url = "node58299-jiramet-noderest.proen.app.ruk-com.cloud:11612";
+const base_url = "http://node58299-jiramet-noderest.proen.app.ruk-com.cloud";
 
 // Set the template engine
 app.set('view engine', 'ejs'); //view file .ejs
