7bb0575aa4c6beb0085e35b01bc886faec1326bb
Sencoool <s6506021610037@email.kmutnb.ac.th>
2024-02-20 11:04:06 +0700

    base_url UPD

--- AxiosNodeHtml.js
+++ AxiosNodeHtml.js
@@ -9,7 +9,7 @@ var bodyParser = require('body-parser');
 
 // Base URL for the API
 //const base_url = "https://api.example.com";
-// const base_url = "http://localhost:3000";
+//const base_url = "http://localhost:3000";
 const base_url = "http://node58299-jiramet-noderest.proen.app.ruk-com.cloud";
 
 // Set the template engine
